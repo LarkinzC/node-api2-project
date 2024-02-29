36033d78d02ede7232df269c584ecbea5f80482b
Larkin Campbell <Larkincampbell24@gmail.com>
2024-02-29 13:54:11 -0800

    get works

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,17 +1,42 @@
 // implement your posts router here
 const express = require('express')
+const Post = require('./posts-model')
 const router = express.Router()
 
 router.get('/', (req, res) => {    
-    res.status(200).json("foo")
+    Post.find()
+    .then(found => {
+        res.json(found)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: "The posts information could not be retrieved",
+            err: err.message
+        })
+    })
 })
 
-router.get('/:id', (req, res) => {    
-    
+router.get('/:id', async (req, res) => {    
+    try {
+        const quote = await Post.findById(req.params.id)
+        if (!quote) {
+            res.status(404).json({
+                message: "The post with the specified ID does not exist"
+            }) 
+        } else {
+            res.status(200).json(quote)
+        }
+    } catch(err)  {
+        res.status(500).json({
+            message: "The post information could not be retrieved",
+            error: err.message
+        })
+    }
 
 })
 
-router.post('/', (req, res) => {    
+router.post('/',  (req, res) => {    
+    const quote = req.body
     
 
 })
